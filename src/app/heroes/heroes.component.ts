c14dd6203a50b8aaa792b7ac6dd2546c8f75ed9d
kposada <kposada001@gmail.com>
2024-05-26 17:11:26 -0500

    new services hero and message

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -3,8 +3,10 @@ import { NgFor } from '@angular/common';
 
 
 import { Hero } from './hero';
-import { HEROES } from '../mock-heroes';
 import { HeroDetailsComponent } from '../hero-details/hero-details.component';
+import { HeroService } from '../hero.service';
+import { MessageService } from '../message.service';
+
 @Component({
   selector: 'app-heroes',
   standalone: true,
@@ -16,10 +18,25 @@ import { HeroDetailsComponent } from '../hero-details/hero-details.component';
   styleUrl: './heroes.component.css'
 })
 export class HeroesComponent {
-  heroes = HEROES;
+  heroes: Hero[] = [];
   selectedHero?: Hero;
+
+  constructor(
+    private heroService: HeroService, 
+    private messageService: MessageService
+  ){}
+
+  ngOnInit(): void {
+    this.getHeroes();
+  }
 
   onSelect(hero: Hero): void{
     this.selectedHero = hero;
+    this.messageService.add(`Heroes Component: Selected hero id = ${hero.id}`)
+  }
+
+  getHeroes(): void{
+    this.heroService.getHeroes()
+      .subscribe(heroes => this.heroes = heroes);
   }
 }
