d388502ef71f13158f685e1fae21f0000f9e9c78
kposada <kposada001@gmail.com>
2024-06-18 18:13:40 -0500

    auth jwt and lazy loan

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,16 +1,34 @@
 import { Routes } from '@angular/router';
 
 import { HeroesComponent } from './heroes/heroes.component';
-import { DashboardComponent } from './dashboard/dashboard.component';
-import { HeroDetailsComponent } from './hero-details/hero-details.component';
+//import { DashboardComponent } from './dashboard/dashboard.component';
+// import { HeroDetailsComponent } from './hero-details/hero-details.component';
 import { PowersComponent } from './powers/powers.component';
 import { HeroNewComponent } from './hero-new/hero-new.component';
+import { authGuard } from './authentication/auth.guard';
+import { LoginComponent } from './login/login.component';
+import { NoFoundComponent } from './no-found/no-found.component';
 
 export const routes: Routes = [
   {path: '', redirectTo: '/dashboard', pathMatch: 'full'},
-  {path: 'heroes', component: HeroesComponent},
-  {path: 'details/:id', component: HeroDetailsComponent},
-  {path: 'dashboard', component: DashboardComponent},
+  {
+    path: 'heroes',
+    component: HeroesComponent,
+    canActivate: [authGuard],
+  },
+  {
+    path: 'details/:id',
+    loadComponent:() => import('./hero-details/hero-details.component')
+      .then(module => module.HeroDetailsComponent)
+  },
+  {
+    path: 'dashboard', 
+    //lazyload:
+    loadComponent:() => import ('./dashboard/dashboard.component')
+      .then(module => module.DashboardComponent)
+  },
   {path: 'powers', component: PowersComponent},
   {path: 'hero/new', component: HeroNewComponent},
+  {path: 'login', component: LoginComponent},
+  {path: '**', component: NoFoundComponent},
 ];
